2ce3425862097c2cdf21169dd72009f83bef18bb
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-02-05 17:31:01 +0900

    フッターの追加

---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  const nowYear: number = new Date().getFullYear();
+</script>
+
+<template>
+  <v-col class="footer d-flex justify-center align-center mt-12">
+    <p>©2022 - {{ nowYear }}  Yuto Sekiguchi</p>
+  </v-col>
+</template>
+
+<style scoped>
+.footer {
+  position: sticky;
+  top: 100vh;
+  min-height: 10vh;
+}
+p {
+  color: #555;
+  font-weight: bold;
+}
+</style>
