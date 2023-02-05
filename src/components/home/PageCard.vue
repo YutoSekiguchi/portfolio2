a54f3bfcea5f6d83ae0c694fa0defbbfd35b1ec9
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-02-05 19:58:04 +0900

    cardの追加

---
+++ src/components/home/PageCard.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+  import { defineProps } from 'vue';
+
+  export interface Props {
+    title: string,
+    icon: string,
+    description: string,
+  }
+  const props = defineProps<Props>();
+</script>
+
+<template>
+  <v-card 
+    class="card"
+    variant="flat"
+  >
+    <p class="card-title text-center">{{ props.title }}</p>
+    <v-col class="d-flex justify-center py-0">
+      <v-icon
+        :icon=props.icon
+        class="card-icon"
+      />
+    </v-col>
+    <v-col class="card-description py-0">
+      <p>{{ props.description }}</p>
+    </v-col>
+  </v-card>
+</template>
+
+<style scoped>
+p, .card-icon {
+  color: #555;
+}
+.card {
+  width: 280px;
+}
+.card-title{
+  font-size: 2rem;
+  font-weight: bold;
+}
+.card-icon {
+  font-size: 8rem;
+}
+.card-description {
+  font-size: 0.9rem;
+  height: 200px;
+  overflow-y: scroll;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
+  import PageCard from '@/components/home/PageCard.vue';
 </script>
 
 <template>
-  <div class="top">
+  <div class="top mb-12">
     <v-img
       cover
       aspect-ratio="1.7"
@@ -11,10 +12,21 @@
       class="mx-auto"
     />
   </div>
+
+  <v-row class="cards mx-auto justify-space-around">
+    <PageCard 
+      title="About"
+      icon="mdi-account-circle"
+      description="Aboutページは、私に関する情報を書いているページです。氏名や生年月日、所属・経歴、取得しているスキルなどが書かれています。あと、自分の写真も…笑"
+    />
+  </v-row>
 </template>
 
 <style scoped>
 .top {
   background-color: #eeeeee;
+}
+.cards {
+  max-width: 1000px;
 }
 </style>
