2929e07c91cbc9c60d690b6a82e97f98ca1817f7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-02-13 21:17:25 +0900

    ダイアログの表示

---
+++ src/components/products/DetailDialog.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+  import { ProductsType } from '@/settings/productsType';
+  import { defineProps, computed, ComputedRef } from 'vue';
+  import ArticleImage from '../common/ArticleImage.vue';
+  import Chip from '../common/Chip.vue';
+  
+  export interface Props {
+    isDialog: boolean;
+    closeDialog: void;
+    product: ProductsType;
+  }
+  const props = defineProps<Props>();
+  const model: ComputedRef<boolean> = computed(() => props.isDialog);
+  
+  const createPath = (name: string) => {
+    return new URL(`../../assets/${name}`, import.meta.url).href;
+  }
+</script>
+
+<template>
+  <v-dialog
+    transition="dialog-bottom-transition"
+    v-model="model"
+  >
+    <v-card
+      variant="elevated"
+      class="dialog-card bg-white mx-auto"
+    >
+      <v-layout>
+        <v-app-bar
+          flat
+          color="white"
+          density="compact"
+        >
+          <template v-slot:prepend>
+            <v-btn icon="mdi-close" @click="props.closeDialog" class="icon"></v-btn>
+          </template>
+          <v-app-bar-title class="products">Products</v-app-bar-title>
+        </v-app-bar>
+        
+        <v-main class="dialog-card-main">
+          <p class="text-center text-h6 font-weight-bold pt-2 px-5 mb-3">{{ product.title }}</p>
+
+          <ArticleImage
+            :path="createPath(`products/${product.image}.png`)"
+            class="mb-6"
+          />
+          
+          <p class="font-weight-bold text-h5 ml-4">概要</p>
+          <p class="description">{{ product.description }}</p>
+
+          <div class="tags mb-12 ml-2 mt-8">
+            <Chip
+              v-for="(tag, index) in product.tagList"
+              :key="index"
+              :name="tag"
+            />
+          </div>
+        </v-main>
+      </v-layout>
+    </v-card>
+  </v-dialog>
+</template>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,11 +1,24 @@
 <script lang="ts" setup>
   import PageTitle from '@/components/common/PageTitle.vue';
   import ArticleImage from '@/components/common/ArticleImage.vue';
+  import DetailDialog from '@/components/products/DetailDialog.vue';
   import Chip from '@/components/common/Chip.vue';
   import Products from '../settings/products.json';
   import { ProductsType } from '@/settings/productsType';
   import { ref, computed, watch } from 'vue';
   
+
+  const isDialog = ref<boolean>(false);
+  const num = ref<number>(0);
+
+  const openDialog = (index: number) => {
+    num.value = index;
+    isDialog.value = true;
+  }
+
+  const closeDialog = () => {
+    isDialog.value = false;
+  }
   const productsList = ref<ProductsType[]>(Products);
   const createPath =  (name: string) => {
     return new URL(`../assets/${name}`, import.meta.url).href;
@@ -13,6 +26,11 @@
 </script>
 
 <template>
+  <DetailDialog
+    :is-dialog="isDialog"
+    :close-dialog="closeDialog"
+    :product="productsList[num]"
+  />
   <div class="products-main mx-auto">
     <v-row class="products-main-header mx-auto">
       <PageTitle title="Products" />
@@ -28,6 +46,8 @@
           <ArticleImage
             :height="150"
             :path="createPath(`products/${product.image}.png`)"
+            style="cursor: pointer;"
+            @click="openDialog(i)"
           />
         </v-col>
         <v-col cols="12" class="card-title pb-0">
@@ -51,6 +71,7 @@
           <v-btn
             class="px-0"
             variant="text"
+            @click="openDialog(i)"
           >
             <v-icon icon="mdi-open-in-new" class="icon mr-1" />
             <p>詳細へ</p>
